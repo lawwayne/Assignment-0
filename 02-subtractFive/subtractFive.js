2f40e1a494bd1aa932450fb056e958fde943ce54
lawwayne <72894553+lawwayne@users.noreply.github.com>
2020-12-23 01:36:08 -0700

    Completed Tasks

--- 02-subtractFive/subtractFive.js
+++ 02-subtractFive/subtractFive.js
@@ -1,6 +1,9 @@
 function subtractFive(num) {
-  // Insert code here;
+ return num - 5;
 }
+
+let numb = 10;
+console.log(subtractFive(numb)); //should show the value 10
 
 // Do not edit this line;
 module.exports = subtractFive;
