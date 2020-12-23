053df7f016664a0b1f17b07472e26f89fcb97678
lawwayne <72894553+lawwayne@users.noreply.github.com>
2020-12-23 01:51:14 -0700

    Completed Task

--- 04-countOfAllBooleans/countOfAllBooleans.js
+++ 04-countOfAllBooleans/countOfAllBooleans.js
@@ -1,6 +1,19 @@
 function countOfAllBooleans(arr) {
-  // Insert code here;
+
+  let count =0;
+  
+  for (let i=0; i<arr.length; i++)
+  {
+    if(arr[i] === true || arr{i} === false)
+    {
+      count++;
+    }
+  }
+  
+  return count;
+
 }
+
 
 // Do not edit this line;
 module.exports = countOfAllBooleans;
