b93a2ce081322625fc5058afa1783d21e2d34705
lawwayne <72894553+lawwayne@users.noreply.github.com>
2020-12-23 01:30:14 -0700

    Completed Tasks

--- 01-titleCaseEdit/titleCaseEdit.js
+++ 01-titleCaseEdit/titleCaseEdit.js
@@ -1,5 +1,15 @@
 function titleCaseEdit(title) {
-  // Insert code here;
+  
+  let finalTitle = title.split(" ") //this will allow the array to happen
+  
+  .map((word) => {
+    return (word.charAt(0).toUppercase() + wordslice(1))
+  })
+  
+  .join("")
+  
+  return finalTitle;
+  
 }
 
 // Do not edit this line;
