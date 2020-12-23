647a4e737c738615e8603e5de4e618ebb4aee198
lawwayne <72894553+lawwayne@users.noreply.github.com>
2020-12-23 01:38:43 -0700

    Completed Task

--- 03-cutInHalfAndFloor/cutInHalfAndFloor.js
+++ 03-cutInHalfAndFloor/cutInHalfAndFloor.js
@@ -1,5 +1,5 @@
 function cutInHalfAndFloor(num) {
-  // Insert code here;
+  return Math.floor(num / 2); //whatever the input is it should divide by 2 and return the quotient
 }
 
 // Do not edit this line;
