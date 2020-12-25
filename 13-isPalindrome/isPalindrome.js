1b5b2381c1ecc002497ccab36195216a0f241319
lawwayne <72894553+lawwayne@users.noreply.github.com>
2020-12-24 18:48:32 -0700

    Completed Task

--- 13-isPalindrome/isPalindrome.js
+++ 13-isPalindrome/isPalindrome.js
@@ -1,5 +1,14 @@
 function isPalindrome(word) {
-  // Insert code here;
+  
+  var pdrome = false;
+  for(var i =0; i < word.length; i++;)
+  {
+    if(word[i] == word[(i+word.length) - 1])
+    {
+      pdrome = true;
+    }
+  }
+  return pdrome;
 }
 
 // Do not edit this line;
