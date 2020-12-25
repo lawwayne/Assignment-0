3939f9c4cc486bac546411f1f34dac0310a6cf3e
lawwayne <72894553+lawwayne@users.noreply.github.com>
2020-12-24 18:53:04 -0700

    Completed Task

--- 14-frequencyCounter/frequencyCounter.js
+++ 14-frequencyCounter/frequencyCounter.js
@@ -1,5 +1,12 @@
 function frequencyCounter(word) {
-  // Insert code here;
+  let count  = {};
+  
+  for (let i=0; i<word.length; i++)
+  {
+    count[word[i]] = (count[word[i]] || 0) +1
+  }
+ 
+  return count;
 }
 
 // Do not edit this line;
