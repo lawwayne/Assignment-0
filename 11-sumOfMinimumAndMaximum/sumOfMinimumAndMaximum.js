f142afa7933c83212ec00e5dfae96f012cf1bfd5
lawwayne <72894553+lawwayne@users.noreply.github.com>
2020-12-24 18:32:10 -0700

    Completed Tasks

--- 11-sumOfMinimumAndMaximum/sumOfMinimumAndMaximum.js
+++ 11-sumOfMinimumAndMaximum/sumOfMinimumAndMaximum.js
@@ -1,6 +1,28 @@
 function sumOfMinimumAndMaximum(nums) {
-  // Insert code here;
+
+  let min = nums[0];
+  let max = nums[0];
+  
+  
+  for(let i=1;i<nums.length;i++)
+  {
+    if(nums[i] > nums[i+1])
+      {
+        min = nums[i];
+      }
+    else if(nums[i] < nums[i+1])
+    {
+      max = nums[i+1];
+    }
+  
+     
+  }
+
+  return max + min;
+
 }
+
+
 
 // Do not edit this line;
 module.exports = sumOfMinimumAndMaximum;
