7e016f751c2fdceea876fe5e8e0a13a801cd939f
lawwayne <72894553+lawwayne@users.noreply.github.com>
2020-12-24 18:08:52 -0700

    Completed Tasks

--- 08-sumOfAllEvenNumbers/sumOfAllEvenNumbers.js
+++ 08-sumOfAllEvenNumbers/sumOfAllEvenNumbers.js
@@ -1,5 +1,15 @@
 function sumOfAllEvenNumbers(nums) {
-  // Insert code here;
+  
+  let count = 0;
+  
+  for (let x=0; x < nums.length; x++)
+  {
+    if (nums[x] % 2 == 0)
+    {
+      count ++;
+    }
+  }
+  return count;
 }
 
 // Do not edit this line;
