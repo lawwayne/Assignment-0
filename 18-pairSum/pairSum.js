02bdb4e96db0c6ada8ba81ca0c05a2e878312991
lawwayne <72894553+lawwayne@users.noreply.github.com>
2020-12-24 19:09:29 -0700

    Complete Task

--- 18-pairSum/pairSum.js
+++ 18-pairSum/pairSum.js
@@ -1,5 +1,21 @@
 function pairSum(nums, target) {
-  // Insert code here;
+  
+  if (nums.lenght<=1)
+  {
+    throw "error";
+  }
+  
+    for(let i = 0; i < nums.length; i++)
+    {
+      for(let j = nums.length; j >i; j--)
+      {
+        return true;
+      }
+
+
+    }
+  return false;
+  
 }
 
 // Do not edit this line;
